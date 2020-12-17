472d52631f6929e93ee291292067a7dea8e28da2
liehuozhizun <hyuan3@ucsc.edu>
2020-12-16 18:13:48 -0800

    Simulator: enable setting target feature

--- src/simulator/simulation/Simulator.java
+++ src/simulator/simulation/Simulator.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 public class Simulator {
     private boolean hideTarget = false;
     private boolean testPurpose = false;
+    private boolean targetFlag = false;
+    private char[] targetValue;
 
     private final Result result = new Result();
 
@@ -21,11 +23,17 @@ public class Simulator {
             result.setMaxTries(Integer.parseInt(arg.substring(5)));
         } else if (arg.startsWith("-test=")) {
             testPurpose = Boolean.parseBoolean(arg.substring(6));
+        } else if (arg.startsWith("-target=")) {
+            targetValue = arg.substring(8).replaceAll(" ", "").toCharArray();
+            targetFlag = targetValue.length == 3
+                    && Character.isDigit(targetValue[0])
+                    && Character.isDigit(targetValue[1])
+                    && Character.isDigit(targetValue[2]);
         }
     }
 
     public Result simulate() {
-        char[] target = TargetGenerator.generate();
+        char[] target = targetFlag ? targetValue : TargetGenerator.generate();
         if (!testPurpose && !hideTarget) System.out.println("\n-- Target: " + target[0] + " " + target[1] + " " + target[2] + "\n");
 
         while (true) {
@@ -39,7 +47,7 @@ public class Simulator {
         if (!testPurpose) System.out.print("> Enter your try (space between each digit): ");
         Scanner scanner = new Scanner(System.in);
         char[] input = scanner.next().replaceAll(" ", "").toCharArray();
-        if (input.length == 1 && input[0] == 'q') throw new RuntimeException("FORCE QUIT GAME");
+        if (input.length == 1 && input[0] == 'q') return true;
         if (input.length != 3
                 || !Character.isDigit(input[0])
                 || !Character.isDigit(input[1])
